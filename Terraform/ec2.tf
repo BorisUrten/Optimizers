ca6893ec252b95f332a5bc7a01fca7ab82a318d0
nickchim98 <nicholaschim02@gmail.com>
2023-11-02 13:12:17 -0400

    Terraform folder

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -32,14 +32,14 @@ resource "aws_security_group" "my_security_group" {
   vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
-    from_port   = 0  
+    from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0  
+    from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
