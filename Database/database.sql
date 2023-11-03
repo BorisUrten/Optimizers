31734b9f829800c3b8a583dc238301cc0272fde1
Boris Urten <borisurtenov@gmail.com>
2023-11-03 19:57:55 -0400

    that is all

---
+++ Database/database.sql
@@ -0,0 +1,8 @@
+create database 023DO-SP23;
+use 023DO-SP23;
+create table Optimizers (    
+    first_name char(25) NOT NULL, 
+    last_name char(30) NOT NULL,                                   
+    );
+
+INSERT INTO `Optimizers` VALUES ('Patricia','Bolaines'),('Munira','Kenjaeva'),('Shihab','Khan'),('Nicholas','Chim'),('Boris','Urtenov');
